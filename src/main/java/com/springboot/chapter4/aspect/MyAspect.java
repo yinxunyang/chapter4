19e65d53b956d161217a6e70b81c28f9c50972d3
yinxunyang <yinxunyang@bestvike.com>
2019-05-11 11:16:33 +0800

    添加环绕切面

--- src/main/java/com/springboot/chapter4/aspect/MyAspect.java
+++ src/main/java/com/springboot/chapter4/aspect/MyAspect.java
@@ -1,8 +1,10 @@
 package com.springboot.chapter4.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -31,5 +33,12 @@ public class MyAspect {
     @AfterThrowing("pointcut()")
     public void AfterThrowing() {
         System.out.println("AfterThrowing......");
+    }
+
+    @Around("pointcut()")
+    public void around(ProceedingJoinPoint jp) throws Throwable {
+        System.out.println("around before......");
+        jp.proceed();
+        System.out.println("around after......");
     }
 }
